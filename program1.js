ed351b18c9786b202fefde04ea084edd60e7a8a6
bhaveshbahad <bahadbhavesh@gmail.com>
2024-04-25 13:40:02 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,4 +4,19 @@ function longestSubstring(s) {
 
 module.exports = { longestSubstring };
 
+function lengthOfLongestSubstring(s) {
+    let maxLength = 0;
+    let start = 0;
+    const charIndexMap = {};
 
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (charIndexMap[char] !== undefined && charIndexMap[char] >= start) {
+            start = charIndexMap[char] + 1;
+        }
+        charIndexMap[char] = i;
+        maxLength = Math.max(maxLength, i - start + 1);
+    }
+
+    return maxLength;
+}
