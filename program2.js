62c01bfcae89cade1653fa102c7b41eed83d55d1
bhaveshbahad <bahadbhavesh@gmail.com>
2024-04-25 13:45:24 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -16,17 +16,3 @@ for (let i = 1; i <= nums.length + 1; i++) {
 }
 
 module.exports = smallestMissingPositiveInteger;
-
-function smallest_missing_positive_integer(nums) {
-  if (!nums.length) {
-      return 1;
-  }
-  
-  const numsSet = new Set(nums);
-  
-  for (let i = 1; i <= nums.length + 1; i++) {
-      if (!numsSet.has(i)) {
-          return i;
-      }
-  }
-}
