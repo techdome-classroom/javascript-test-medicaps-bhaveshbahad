0e73b797338cd54c2f1814838ceda3d83634d48a
bhaveshbahad <bahadbhavesh@gmail.com>
2024-04-25 13:45:19 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,17 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  if (!nums.length) {
+    return 1;
+}
+
+const numsSet = new Set(nums);
+
+for (let i = 1; i <= nums.length + 1; i++) {
+    if (!numsSet.has(i)) {
+        return i;
+    }
+}
 
 }
 
